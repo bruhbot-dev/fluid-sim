ae3ea224549cf2905b5e01827fd17eba6202cb59
Oliver Johnson <oliverjohnson@pc-135-89.customer.ask4.lan>
2023-05-16 04:33:13 +0100

    created app.java

---
+++ app.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+
+class App {
+
+    private static void initWindow() {
+        // create a window frame and set the title in the toolbar
+        JFrame window = new JFrame("Can't Stop, Won't Stop, GameStop");
+        // when we close the window, stop the app
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        
+        // don't allow the user to resize the window
+        window.setResizable(true);
+        // fit the window size around the components (just our jpanel).
+        // pack() should be called after setResizable() to avoid issues on some platforms
+        window.pack();
+        // open window in the center of the screen
+        window.setLocationRelativeTo(null);
+        // display the window
+        window.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        // invokeLater() is used here to prevent our graphics processing from
+        // blocking the GUI. https://stackoverflow.com/a/22534931/4655368
+        // this is a lot of boilerplate code that you shouldn't be too concerned about.
+        // just know that when main runs it will call initWindow() once.
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                initWindow();
+            }
+        });
+    }
+}
