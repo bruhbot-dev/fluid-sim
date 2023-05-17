921124d3b34fa7d40a9a2272231229ab431f9633
bruhbot-dev <olivergeorgejames@gmail.com>
2023-05-18 00:31:55 +0100

    changes to test.java

--- test.java
+++ test.java
@@ -7,6 +7,12 @@ public class test {
 
     Vector2 resultVec = testVec.Add(testVec2);
     System.out.println(resultVec.print());
+
+    Vector2 testVec3 = new Vector2(13, -2);
+    Vector2 testVec4 = new Vector2(16, -7);
+    Vector2 resultVec2 = testVec3.Sub(testVec4);
+    System.out.println(resultVec2.print());
+
     }
 
 
