9adb6e2578a81795ff86b206d2e79f52597991cb
bruhbot-dev <olivergeorgejames@gmail.com>
2023-05-17 19:31:45 +0100

    made a test file

---
+++ test.java
@@ -0,0 +1,12 @@
+public class test {
+    public static void main(String[] args) {
+        
+    
+    Vector2 testVec = new Vector2(10, -5);
+    Vector2 testVec2 = new Vector2(10, -5);
+
+    Vector2 resultVec = testVec.Add(testVec2);
+    }
+
+
+}
