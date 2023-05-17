ae8898d984afd63de33b57f366216b93d7c112e1
bruhbot-dev <olivergeorgejames@gmail.com>
2023-05-17 19:12:24 +0100

    defined behaviour for add and sub

--- Vector2.java
+++ Vector2.java
@@ -1,8 +1,37 @@
-public class Vector {
-    float x;
-    float y;
-    PVector(float x, float y) {
-     this.x = x;
-     this.y = y;
-    }
+public class Vector2 {
+
+   private double component1;
+   private double component2;
+
+   
+
+   public Vector2(double component1, double component2) {
+      this.component1 = component1;
+      this.component2 = component2;
+   }
+
+   public Vector2 Add(Vector2 Vec2){
+
+      double component1 = this.component1 + Vec2.component1;
+      double component2 = this.component2 + Vec2.component2;
+
+      Vector2 returnVector  = new Vector2(component1, component2);
+      return returnVector;
+  }
+
+   Vector2 Sub(Vector2 Vec1, Vector2 Vec2){
+
+      double component1 = this.component1 - Vec2.component1;
+      double component2 = this.component2 - Vec2.component2;
+
+      Vector2 returnVector  = new Vector2(component1, component2);
+      return returnVector;
+   }
+
+   /*double Multi(Vector2 Vec1, Vector2 Vec2){
+
+   }  */
+   
+   //dotProduct()
+
    }
