bb17ce6968854fa3297bd91d71816d5cf4d89a23
bruhbot-dev <olivergeorgejames@gmail.com>
2023-05-18 00:20:21 +0100

    added a print method

--- Vector2.java
+++ Vector2.java
@@ -3,11 +3,16 @@ public class Vector2 {
    private double component1;
    private double component2;
 
-   
 
    public Vector2(double component1, double component2) {
       this.component1 = component1;
       this.component2 = component2;
+   }
+
+   public String print(){
+      double output = this.component1 + this.component2;
+      String stringOutput = String.valueOf(output);
+      return stringOutput;
    }
 
    public Vector2 Add(Vector2 Vec2){
@@ -19,7 +24,7 @@ public class Vector2 {
       return returnVector;
   }
 
-   Vector2 Sub(Vector2 Vec1, Vector2 Vec2){
+   public Vector2 Sub(Vector2 Vec1, Vector2 Vec2){
 
       double component1 = this.component1 - Vec2.component1;
       double component2 = this.component2 - Vec2.component2;
