806d106cf200b49fcdac9b3a9c83a65055916631
bruhbot-dev <olivergeorgejames@gmail.com>
2023-05-17 01:53:37 +0100

    added move

--- ball.java
+++ ball.java
@@ -1,5 +1,23 @@
 public class ball{
+    private float _size;
+    private _PVector _location;
+
     void collisionDetection() {
         
     }
+
+    void move(PVector velocity, float colliderSize) {
+        _location.add(velocity);
+        if (checkBounce(_location.x, colliderSize)) {
+          velocity.x = velocity.x * -1;
+        }
+          
+        if (checkBounce(_location.y, colliderSize)) {
+          velocity.y = velocity.y * -1;
+        }
+        
+        if (checkBounce(_location.z, colliderSize)) {
+          velocity.z = velocity.z * -1;
+        }
+      }
 }
