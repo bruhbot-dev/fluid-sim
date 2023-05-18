62c17c1cfd480ba11d57bdebe94dba01515f715b
bruhbot-dev <olivergeorgejames@gmail.com>
2023-05-18 02:35:53 +0100

    filled in calcAccel

--- ball.java
+++ ball.java
@@ -2,15 +2,27 @@
 public class ball{
     private float radius;
     private Vector2 location;
-    private Vector2 sumForces;
+    public Vector2 sumForces;
     private double mass;
 
 
-    void collisionDetection() {
-        
+    
+    public ball(float radius, Vector2 location, Vector2 sumForces, double mass) {
+        this.radius = radius;
+        this.location = location;
+        this.sumForces = sumForces;
+        this.mass = mass;
+        System.out.println("test");
     }
 
-    public void calcAccel(this.sumForces){
+
+    public Vector2 calcAccel(){
+        double accelComponent1 = this.sumForces.component1 / mass;
+        double accelComponent2 = this.sumForces.component2 / mass;
+        
+        Vector2 accelVec = new Vector2(accelComponent1, accelComponent2);
+        System.out.println("test3");
+        return accelVec;
 
     }
 }
