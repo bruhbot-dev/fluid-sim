a9f9cf932219a4d068eb95fe45584ee40487933b
bruhbot-dev <olivergeorgejames@gmail.com>
2023-05-18 02:36:10 +0100

    added stuff to test calcAccel

--- test.java
+++ test.java
@@ -6,12 +6,18 @@ public class test {
     Vector2 testVec2 = new Vector2(10, -5);
 
     Vector2 resultVec = testVec.Add(testVec2);
-    System.out.println(resultVec.print());
+   // System.out.println(resultVec.print());
 
     Vector2 testVec3 = new Vector2(13, -2);
     Vector2 testVec4 = new Vector2(16, -7);
     Vector2 resultVec2 = testVec3.Sub(testVec4);
-    System.out.println(resultVec2.print());
+    //System.out.println(resultVec2.print());
+
+
+    ball testBall = new ball(1, testVec4, resultVec2, 1);
+    
+    Vector2 accelResultVec = testBall.calcAccel();
+    System.out.println(accelResultVec.print());
 
     }
 
