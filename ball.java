95254b49c8b870e88502313419da0503ab7990c7
bruhbot-dev <olivergeorgejames@gmail.com>
2023-05-18 01:48:42 +0100

    removed old method added calcAccel

--- ball.java
+++ ball.java
@@ -1,25 +1,16 @@
 
 public class ball{
     private float radius;
-    private positionVector location;
-    private sumForces
+    private Vector2 location;
+    private Vector2 sumForces;
+    private double mass;
+
 
     void collisionDetection() {
         
     }
 
-    void move(PVector velocity, float colliderSize) {
-        _location.add(velocity);
-        if (checkBounce(_location.x, colliderSize)) {
-          velocity.x = velocity.x * -1;
-        }
-          
-        if (checkBounce(_location.y, colliderSize)) {
-          velocity.y = velocity.y * -1;
-        }
-        
-        if (checkBounce(_location.z, colliderSize)) {
-          velocity.z = velocity.z * -1;
-        }
-      }
+    public void calcAccel(this.sumForces){
+
+    }
 }
