66eb0c7621c9d91d14926dec7e49e68f0c48b5f0
bruhbot-dev <olivergeorgejames@gmail.com>
2023-05-20 10:44:29 +0100

    updated the test thingy

--- test.java
+++ test.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+
 public class test {
     public static void main(String[] args) {
-        
+    
+    Vector2 emptyVec = new Vector2(0, 0);
     
     Vector2 testVec = new Vector2(10, -5);
     Vector2 testVec2 = new Vector2(10, -5);
@@ -14,10 +17,18 @@ public class test {
     //System.out.println(resultVec2.print());
 
 
-    ball testBall = new ball(1, testVec4, resultVec2, 1);
+    ball testBall = new ball(1, emptyVec, resultVec2, 1);
     
     Vector2 accelResultVec = testBall.calcAccel();
     System.out.println(accelResultVec.print());
+
+    ArrayList<Vector2> testArray = new ArrayList<Vector2>();
+    testArray.add(0, testVec);
+    testArray.add(1, testVec2);
+
+    testBall.movePos(testArray);
+    testBall.movePos(testArray);
+    testBall.movePos(testArray);
 
     }
 
