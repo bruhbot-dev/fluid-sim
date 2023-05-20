2bbd5e0b0f7d1ee9126444c1989945e7d4e870f3
bruhbot-dev <olivergeorgejames@gmail.com>
2023-05-20 10:44:08 +0100

    got movePos working

--- ball.java
+++ ball.java
@@ -4,20 +4,20 @@ import javax.print.attribute.standard.Copies;
 
 public class ball{
     private float radius;
-    private Vector2 position;
+    private Vector2 position = new Vector2(0, 0);
     public Vector2 sumForces;
     private double mass;
-    private Vector2 velocity ;
+    private Vector2 velocity = new Vector2(0, 0);
     private Vector2 accel; 
+    private Vector2 copySumForces;
 
 
     
-    public ball(float radius, Vector2 location, Vector2 sumForces, double mass) {
+    public ball(float radius, Vector2 position, Vector2 sumForces, double mass) {
         this.radius = radius;
-        this.location = location;
+        this.position = position;
         this.sumForces = sumForces;
         this.mass = mass;
-        System.out.println("test");
     }
 
 
@@ -26,31 +26,33 @@ public class ball{
         double accelComponent2 = this.sumForces.component2 / mass;
         
         Vector2 accelVec = new Vector2(accelComponent1, accelComponent2);
-        System.out.println("test3");
         return accelVec;
 
     }
 
     public Vector2 calcSumForces(ArrayList<Vector2> forceArray){
-        Vector2 copySumForces = this.sumForces;
+        this.copySumForces = this.sumForces;
         for (Vector2 i: forceArray) {
             copySumForces.Add(i);
-          
-          return copySumForces;
         }
 
+        return copySumForces;    
+    }
+
     public void calcVeloc(){
-        this.velocity.component1 = this.velocity.component1 * this.accel.component1;
-        this.velocity.component2 = this.velocity.component2 * this.accel.component2;
+        this.velocity.component1 = this.velocity.component1 + this.accel.component1;
+        this.velocity.component2 = this.velocity.component2 + this.accel.component2;
 
 
      }
 
     public Vector2 movePos(ArrayList<Vector2> outerforceArray){
+        System.out.println(position.print());
         calcSumForces(outerforceArray);
-        calcAccel();
+        accel = calcAccel();
         calcVeloc();
-
+        this.position = this.position.Add(this.velocity);
+        System.out.println(this.position.print());
         return position;
     }
     
